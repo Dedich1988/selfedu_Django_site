ee47e8d33e891f25139a7b7f918a208e22f2ea28
Dzmitry <dedichpgu@gmail.com>
2023-10-27 22:04:14 +0300

    6. Шаблоны (templates). Начало

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -5,8 +5,9 @@ class Women(models.Model):
     content = models.TextField(blank=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     time_create = models.DateTimeField(auto_now_add=True)
-    title_update = models.DateTimeField(auto_now=True)
+    time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
+
